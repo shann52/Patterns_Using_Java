e5b1bf669387b13aa627ff68455c579a7050d3b3
Your Name <you@example.com>
2026-06-14 13:15:47 +0530

    Added Patter1

--- Pattern1.java
+++ Pattern1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Solution {
+public class Pattern1 {
     public void pattern1(int n) {
         int i,j;
     for(i=0; i<n; i++){
@@ -12,10 +12,11 @@ class Solution {
   }
     }
 public static void main(String [] args){
-  Scanner sc = new Scanner(System.in);
-  Solution sol = new Solution();
-  System.out.println("Enter Your Output:");
-  int value = sc.nextInt();
-  sol.pattern1(value);
-    
-}}
+  try (Scanner sc = new Scanner(System.in)) {
+    Pattern1 sol = new Pattern1();
+    System.out.println("Enter Your Output:");
+    int value = sc.nextInt();
+    sol.pattern1(value);
+  }
+}
+}
